ef4413374dd32f14ddd7b0d68e4c0e6e963773fc
Your Name <you@example.com>
2018-09-17 12:22:57 +0800

    ds

--- config.php
+++ config.php
@@ -1,12 +1,13 @@
 <?php
 //配置文件
 $config = array(
-    "db_host" => "localhost",
+    "db_host" => "138.197.214.52",
     "db_username" => "root",
-    "db_password" => "root",
-    "db_port"=>"8889",	
-    "db_name" => "xcp",
+    "db_password" => "123123",
+    "db_port"=>"3306",
+    "db_name" => "wafdata",
     "page_size" => "20",
     "allow" => array("c" => "user", "a"=>array("register", "create", "login", "do_login"))
 
 );
+
